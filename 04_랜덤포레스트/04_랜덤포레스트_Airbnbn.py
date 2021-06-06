4304060ecbeaca2c7dbd09593f6e3de02174447d
leedozed <leedozed@gmail.com>
2021-06-06 19:42:37 +0900

    1

---
+++ 04_랜덤포레스트/04_랜덤포레스트_Airbnbn.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_excel('airbnb.xlsx')
+# pritn(data)
+
+#리뷰가 11개 이상인 데이터만 사용
+
+
+
+
+
